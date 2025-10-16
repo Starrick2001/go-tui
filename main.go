2f399b81f556bd740e3ceea61160b06a0f527af0
Starrick2001 <lebuidihoa257@gmail.com>
2025-10-16 23:34:05 +0700

    init

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(NewModel())
+	_, err := p.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type AppModel struct {
+	title     string
+	textInput textinput.Model
+}
+
+func (a *AppModel) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (a *AppModel) Update(tea.Msg) (tea.Model, tea.Cmd) {
+	return a, nil
+}
+
+func (a *AppModel) View() string {
+	return a.textInput.View()
+}
+
+func NewModel() tea.Model {
+	tI := textinput.New()
+	tI.Placeholder = "Enter text"
+	tI.Focus()
+	return &AppModel{title: "Title", textInput: textinput.New()}
+}
